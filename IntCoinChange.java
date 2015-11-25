85d6699d1b8e0a1d2f67d531dfcffcbfd38a59ef
Radu Siminiceanu <radu@buddy.com>
2015-11-25 10:54:29 -0800

    style

--- IntCoinChange.java
+++ IntCoinChange.java
@@ -3,13 +3,13 @@ import java.util.*;
 public class IntCoinChange {
 
     private static int numCoins(Integer[] l) {
-        return l==null ? Integer.MAX_VALUE - 1 :
+        return l==null ? Integer.MAX_VALUE - 1 : // -1 to make sure 1+numCoins does not overflow
                 Arrays.asList(l).stream().mapToInt(x->x).sum();
     }
 
     private static Integer[] computeSolution(List<Integer> coinValues, int S, Map<Integer, Integer[]> cache) {
         // base case 1: invalid target sum
-        if (S<0) return null;
+        if (S < 0) return null;
         // base case 2: already computed
         Integer[] cached = cache.get(S);
         if (cached != null) { return cached; }
@@ -17,16 +17,16 @@ public class IntCoinChange {
         int n = coinValues.size();
         Integer[] solution = new Integer[n];
         Arrays.fill(solution, 0);
-        if (S==0) { return solution; }
+        if (S == 0) { return solution; }
 
         // Dynamic Programming technique:
-        // solution(S) = 1 + min {i=1..n | solution(S-v_i)}
+        // solution(S) = 1 + min {i=0..n-1 | solution(S - v_i)}
         int min = Integer.MAX_VALUE - 1;
         for (int i=0; i<n; i++) {
             int v = coinValues.get(i);
             Integer[] partialSolution = computeSolution(coinValues, S - v, cache);
             int thisNumCoins = numCoins(partialSolution);
-            if (1 + thisNumCoins< min) {
+            if (1 + thisNumCoins < min) {
                 solution = Arrays.copyOf(partialSolution, n);
                 solution[i]++;
                 min = 1 + thisNumCoins;
